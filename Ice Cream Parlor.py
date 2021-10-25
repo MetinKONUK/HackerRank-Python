3cabd900aa7a64f04bc83b668227c08df37fc754
Metin <40642973+MetinKONUK@users.noreply.github.com>
2021-10-25 12:55:05 +0300

    Ice Cream Parlor.py
    The solution of Ice Cream Parlor problem in Problem Solving category on HackerRank

---
+++ Ice Cream Parlor.py
@@ -0,0 +1,5 @@
+def icecreamParlor(m, arr):
+    for i in range(len(arr)-1):
+        for j in range(i+1, len(arr)):
+            if arr[i] + arr[j] == m: return [i+1, j+1]
+#Simple, try all possible pairs
